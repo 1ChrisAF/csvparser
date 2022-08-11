9a8f308bd97cfa89fbec9ec12a592a08d6d7672c
Christopher Flippo <christopher.allen.flippo@gmail.com>
2022-08-11 17:52:58 -0500

    IT WORKS. IT REALLY WORKS.

---
+++ dataObject.js
@@ -0,0 +1,26 @@
+class dataObject {
+    seq;
+    name_first;
+    name_last;
+    age;
+    street;
+    city;
+    state;
+    zip;
+    dollar;
+    pick;
+    date;
+    constructor(a,b,c,d,e,f,g,h,i,j,k) {
+        this.seq = a;
+        this.name_first = b;
+        this.name_last = c;
+        this.age = d;
+        this.street = e;
+        this.city = f;
+        this.state = g;
+        this.zip = h;
+        this.dollar = i;
+        this.pick = j;
+        this.date = k;
+    }
+}
---
+++ parser.js
@@ -0,0 +1,72 @@
+
+class dataObject {
+    seq;
+    name_first;
+    name_last;
+    age;
+    street;
+    city;
+    state;
+    zip;
+    dollar;
+    pick;
+    date;
+    constructor(a,b,c,d,e,f,g,h,i,j,k) {
+        this.seq = a;
+        this.name_first = b;
+        this.name_last = c;
+        this.age = d;
+        this.street = e;
+        this.city = f;
+        this.state = g;
+        this.zip = h;
+        this.dollar = i;
+        this.pick = j;
+        this.date = k;
+    }
+    
+}
+const CSVtoArray = function(text) {
+    let p = '', row = [''], ret = [row], i = 0, r = 0, s = !0, l;
+    for (l of text) {
+        if ('"' === l) {
+            if (s && l === p) row[i] += l;
+            s = !s;
+        } else if (',' === l && s) l = row[++i] = '';
+        else if ('\n' === l && s) {
+            if ('\r' === p) row[i] = row[i].slice(0, -1);
+            row = ret[++r] = [l = '']; i = 0;
+        } else row[i] += l;
+        p = l;
+    }
+    return ret;
+}
+let fs = require('fs');
+require('./dataObject.js');
+let data = fs.readFileSync('convertcsv.csv');
+dataString = data.toString();
+let dataArray = CSVtoArray(dataString);
+// let headers = dataArray[0];
+let objectArray = [];
+for (let z = 1; z < dataArray.length; z++) {
+    let a = dataArray[z][0];
+    let b = dataArray[z][1];
+    let c = dataArray[z][2];
+    let d = dataArray[z][3];
+    let e = dataArray[z][4];
+    let f = dataArray[z][5];
+    let g = dataArray[z][6];
+    let h = dataArray[z][7];
+    let i = dataArray[z][8];
+    let j = dataArray[z][9];
+    let k = dataArray[z][10];    
+    let newObject = new dataObject(a,b,c,d,e,f,g,h,i,j,k);
+    objectArray.push(newObject);
+}
+/*
+let fileText = '';
+for (let i = 0; i < objectArray.length; i++) {
+    fileText += JSON.stringify(objectArray[i]) + '\n';
+}
+*/
+fs.writeFileSync('parsedData.txt',JSON.stringify(objectArray));
